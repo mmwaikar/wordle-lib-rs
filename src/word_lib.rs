af8aa45a5018de77f071b01e22aca9c53b75efc8
Manoj Waikar <mmwaikar@gmail.com>
2025-07-17 15:05:42 +0200

    Add tests to the word_lib.

--- src/word_lib.rs
+++ src/word_lib.rs
@@ -1,6 +1,7 @@
 use rand::Rng;
 
-const WORD_LENGTH: usize = 5;
+use crate::models::constants::WORD_LENGTH;
+
 const ALL_WORDS: &str = include_str!("words.txt");
 
 pub fn get_5_letter_words() -> Vec<String> {
@@ -29,4 +30,33 @@ fn sanitize_word(word: &str) -> String {
         .chars()
         .filter(|c| c.is_ascii_alphabetic())
         .collect()
+}
+
+#[cfg(test)]
+mod test_super {
+    use super::*;
+
+    #[test]
+    fn test_get_5_letter_words() {
+        let five_letter_words = get_5_letter_words();
+        assert!(!five_letter_words.is_empty(), "The list of words should not be empty");
+
+        let mut rng = rand::rng();
+        let n = rng.random_range(1..=five_letter_words.len());
+        let word = &five_letter_words[n];
+        assert_eq!(word.len(), WORD_LENGTH, "The word should be 5 letters long");
+    }
+
+    #[test]
+    fn test_get_random_word() {
+        let word = get_random_word();
+        assert_eq!(word.len(), WORD_LENGTH, "The word should be 5 letters long");
+    }
+
+    #[test]
+    fn test_is_valid_word() {
+        let word = get_random_word();
+        let valid = is_valid_word(&word);
+        assert_eq!(valid, true, "The randomly generated word should be a valid one");
+    }
 }
